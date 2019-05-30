bc8160c8a0c119e4d0de41d31d4e56daec1d0bb8
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-05-30 08:58:42 +0100

    Add project files.

---
+++ DefaultcCreditCardML/Card.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefaultcCreditCardML
+{
+
+    public class CreditCardData
+    {
+        [LoadColumn(1)]
+        public float LIMIT_BAL;
+        [LoadColumn(2)]
+        public float SEX;
+        [LoadColumn(3)]
+        public float EDUCATION;
+        [LoadColumn(4)]
+        public float MARRIAGE;
+
+        [LoadColumn(5)]
+        public float AGE;
+
+        [LoadColumn(6)]
+        public float PAY_1;
+
+        [LoadColumn(7)]
+        public float PAY_2;
+
+        [LoadColumn(8)]
+        public float PAY_3;
+
+        [LoadColumn(9)]
+        public float PAY_4;
+
+        [LoadColumn(10)]
+        public float PAY_5;
+
+        [LoadColumn(11)]
+        public float PAY_6;
+
+        [LoadColumn(12)]
+        public float BILL_AMT1;
+
+
+        [LoadColumn(13)]
+        public float BILL_AMT2;
+
+        [LoadColumn(14)]
+        public float BILL_AMT3;
+
+        [LoadColumn(15)]
+        public float BILL_AMT4;
+        [LoadColumn(16)]
+        public float BILL_AMT5;
+        [LoadColumn(17)]
+        public float BILL_AMT6;
+
+        //ID,LIMIT_BAL,SEX,EDUCATION,MARRIAGE,AGE,
+        //PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6,
+        //BILL_AMT1,BILL_AMT2,BILL_AMT3,BILL_AMT4,BILL_AMT5,BILL_AMT6,
+        //PAY_AMT1,PAY_AMT2,PAY_AMT3,PAY_AMT4,PAY_AMT5,PAY_AMT6,default payment next month
+
+        [LoadColumn(18)]
+        public float PAY_AMT1;
+
+        [LoadColumn(19)]
+        public float PAY_AMT2;
+        [LoadColumn(20)]
+        public float PAY_AMT3;
+        [LoadColumn(21)]
+        public float PAY_AMT4;
+        [LoadColumn(22)]
+        public float PAY_AMT5;
+
+        [LoadColumn(23)]
+        public float PAY_AMT6;
+        [LoadColumn(24), ColumnName("Label")]
+        public bool isDefault;
+    }
+
+    public class CreditCardPrediction : CreditCardData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        //public float Probability { get; set; }
+
+        //public float Score { get; set; }
+    }
+}
---
+++ DefaultcCreditCardML/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace DefaultcCreditCardML
+{
+    class Program
+    {
+
+        static string dataPath = "creditcard.csv";
+        static void Main(string[] args)
+        {
+            // MLCreditCard();
+            MLCreditCard();
+        }
+
+        private static void MLCreditCard()
+        {
+            Console.WriteLine("Different Credit Card Algorithms evaluation");
+
+            string[] InputColumns;
+            MLContext ctx;
+            DataOperationsCatalog.TrainTestData splitData;
+            LoadandSplitData(out InputColumns, out ctx, out splitData);
+
+
+            //create single features column
+            var concatenatingEstimator = ctx.Transforms.Concatenate("Features", InputColumns);
+
+
+            //select algorithm
+            var estimator = concatenatingEstimator.Append(ctx.BinaryClassification.Trainers.FastForest("Label", "Features"));
+
+            //train model
+            var model = estimator.Fit(splitData.TrainSet);
+
+            var predictions = model.Transform(splitData.TestSet);
+
+            var metrics = ctx.BinaryClassification.EvaluateNonCalibrated(predictions, "Label");
+
+
+            Console.WriteLine($"Accuracy : {metrics.Accuracy.ToString("0.00")} + F1 Score: {metrics.F1Score.ToString("0.00")} ");
+            Console.WriteLine($"Negative Precision : {metrics.NegativePrecision.ToString("0.00")} + Negative Recall : {metrics.NegativeRecall.ToString("0.00")}");
+            Console.WriteLine($"Positive Precision :{metrics.PositivePrecision.ToString("0.00")} + Positive Recall : {metrics.NegativePrecision.ToString("0.00")}");
+            Console.WriteLine($"AUPRC :{metrics.AreaUnderPrecisionRecallCurve.ToString("0.00")} + AURC : {metrics.AreaUnderRocCurve.ToString("0.00")}");
+            
+            Console.Read();
+            //  Ml();
+        }
+
+        private static void LoadandSplitData(out string[] InputColumns, out MLContext ctx, out DataOperationsCatalog.TrainTestData splitData)
+        {
+            InputColumns = (new CreditCardData()).GetType().GetFields().Where(x => x.FieldType != typeof(bool)).Select(x => x.Name).ToArray();
+
+
+            //Step 1. Create a ML Context
+            ctx = new MLContext();
+
+            //Step 2. Read in the input data for model training
+            IDataView dataReader = ctx.Data
+                .LoadFromTextFile<CreditCardData>(dataPath, separatorChar: ',', hasHeader: true);
+
+
+            //split data
+            splitData = ctx.Data.TrainTestSplit(dataReader, 0.2);
+        }
+
+      public static  void CHeckFeatures(MLContext context, IDataView data)// EstimatorChain<ITransformer> pp)// pipeline)//EstimatorChain<BinaryPredictionTransformer<LinearBinaryModelParameters>> pipeline)//IEstimator<ITransformer> pipeline)
+        {
+            string[] InputColumns;
+            DataOperationsCatalog.TrainTestData splitData;
+            LoadandSplitData(out InputColumns, out context, out splitData);
+            var pipeline = context.Transforms.Concatenate("Features",InputColumns)
+                                   .Append(context.BinaryClassification.Trainers.AveragedPerceptron("Label", "Features"));
+      
+            var model = pipeline.Fit(data);
+            var transformedData = model.Transform(data);
+
+            var linearModel = model.LastTransformer;
+
+            var featureContributionCalculation = context.Transforms.CalculateFeatureContribution(linearModel, normalize: false);
+
+            var featureContributionData = featureContributionCalculation.Fit(transformedData).Transform(transformedData);
+            var featureContributionDataPreview = featureContributionCalculation.Fit(transformedData).Transform(transformedData).Preview();
+
+            var shuffledSubset = context.Data.TakeRows(context.Data.ShuffleRows(featureContributionData), 10);
+            var scoringEnumerator = context.Data.CreateEnumerable<CreditCardData>(shuffledSubset, true);
+
+            foreach (var row in scoringEnumerator)
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.Read();
+        }
+
+    }
+}
